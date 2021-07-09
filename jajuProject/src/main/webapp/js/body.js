c4837e28c9bb60245f39c0cb3a30ff52974fb42d
DESKTOP-2G9U224\82103 <sangming24@naver.com>
2021-07-09 11:43:11 +0900

    인덱스 바디 수정

--- jajuProject/src/main/webapp/js/body.js
+++ jajuProject/src/main/webapp/js/body.js
@@ -6,34 +6,44 @@ $(function(){
 		success: function(data){
 			//alert(JSON.stringify(data));
 			
-			$.each(data.list, function(index, items){
-				$('<tr />').append($('<td />',{
-					align : 'center',
-					text : items.board_seq
-				})).append($('<td />',{
-					
-				}).append($('<a />',{
-					style : 'cursor:pointer;',
-					id : 'b_subject',
-					text : items.board_subject 
-				}))).append($('<td />',{
-					align : 'center',
-					text : items.board_id
-				})).append($('<td />',{
-					align : 'center',
-					text : items.logtime
-				})).append($('<td />',{
-					align : 'center',
-					text : items.board_hit
+			if(data.list.length == 0) {
+				$('<tr />').append($('<td />', {
+					style : 'padding: 150px 0 148px; border-top: 1px solid #e6e6e6; border-bottom: 1px solid #e6e6e6; font-size: 14px; color: #4c4c4c; text-align: center; margin-bottom: 20px;',
+					colspan: '5',
+					text : '게시글이 존재하지 않습니다.'
 				})).appendTo($('#boardTable'));
+			}else{
+				$.each(data.list, function(index, items){
+					$('<tr />', {
+						style : 'border-top: 1px solid #e6e6e6; border-bottom: 1px solid #e6e6e6;'
+					}).append($('<td />',{
+						align : 'center',
+						text : items.board_seq
+					})).append($('<td />',{
+						
+					}).append($('<a />',{
+						style : 'cursor:pointer;',
+						id : 'b_subject',
+						text : items.board_subject 
+					}))).append($('<td />',{
+						align : 'center',
+						text : items.board_id
+					})).append($('<td />',{
+						align : 'center',
+						text : items.logtime
+					})).append($('<td />',{
+						align : 'center',
+						text : items.board_hit
+					})).appendTo($('#boardTable'));
+					
+				});
 				
-			});
-			
-			$(document).on('click', '#b_subject', function(){
-				var seq = $(this).parent().prev().text();
-				
-				location.href = '/jaju/freeboard/freeboardView?board_seq=' + seq + '&pg=1';
-			});
+				$(document).on('click', '#b_subject', function(){
+					var seq = $(this).parent().prev().text();
+					
+					location.href = '/jaju/freeboard/freeboardView?board_seq=' + seq + '&pg=1';
+				});
+			}
 		},
 		error: function(err){
 			console.log(err);
--- jajuProject/src/main/webapp/main/body.jsp
+++ jajuProject/src/main/webapp/main/body.jsp
@@ -54,7 +54,7 @@
 						</tr>
 					</thead>
 					<tbody id="boardTable">
-					
+						
 					</tbody>
 				</table>
 			</div>
